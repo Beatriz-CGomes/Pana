68e53267e0412599ebb3e4c55c7e10d5de1dad20
Beatriz-CGomes <78942393+Beatriz-CGomes@users.noreply.github.com>
2022-04-02 22:10:01 -0300

    pana

---
+++ frontend/src/pages/Sobrenos/index.tsx
@@ -0,0 +1,49 @@
+
+import React from 'react'
+import './sobre.css'
+
+
+
+
+const Sobrenos = () => {
+    return (
+        <>
+
+
+            <div id="conteudo" className="grupo" >
+            <img src="https://imgur.com/ZAQmPp0.png" alt="Banner" className="img-home"/>
+    
+                <h2>Missão</h2>
+                <p>
+                    Nossa missão é impactar positivamente a vida dos refugiados. Temos como objetivo diminuir os problemas que eles enfrentam ao chegar no nosso
+                    país e assim conectamos nosso público as melhores ong’s para diminuir as dificuldades encontradas por eles.
+                </p>
+
+                <br></br>
+
+                <h2>Visão</h2>
+                <p>
+                    Sermos referências até 2027 na promoção de projetos de cidadania ativa e diminuição de desigualdade .</p>
+
+                <br></br>
+
+                <h2>Valores</h2>
+                <p>I - Ética</p>
+                <p>II - Promover a Dignidade e Diversidade</p>
+                <p>III - Cidadania Ativa</p>
+                <p>III - prender e apoiar uns aos outros</p>
+                <p>IV - Diminuir a desiguladade.</p>
+        
+
+
+
+            </div>
+
+
+
+        </>
+
+    )
+}
+
+export default Sobrenos
